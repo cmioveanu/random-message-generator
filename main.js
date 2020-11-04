6f5052f86db59d04f6b345c55183523d9ad18364
Cristian <60708997+cmioveanu@users.noreply.github.com>
2020-11-04 15:34:09 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+/* 
+1. make an object with 3 arrays of words/sentences
+2. make a method in the object that takes random words from
+   each of the array and combines them to output them to the console
+
+*/
